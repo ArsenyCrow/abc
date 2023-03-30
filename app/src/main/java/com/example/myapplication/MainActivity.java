642d74ae00292d1d8005ead1c9433af470b85595
ArsenyCrow <624arseniy@gmail.com>
2023-03-30 17:59:17 +0500

    1.3

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,12 +6,18 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView textView;
+    private TextView plList;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        plList = findViewById(R.id.tv_pl_names);
+        String[] names = {"Java", "Python", "Ruby", "JavaScript", "Matlab"};
+        for(String name : names) {
+            plList.append(name + '\n');
+
     }
+  }
 }
