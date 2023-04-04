80c5b1f585cf4b6525ac17d03000d263dd8aac85
ArsenyCrow <624arseniy@gmail.com>
2023-04-04 17:57:11 +0500

    1.4

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -14,7 +14,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         plList = findViewById(R.id.tv_pl_names);
-        String[] names = {"Java", "Python", "Ruby", "JavaScript", "Matlab"};
+        String[] names = {"Java", "Python", "Ruby", "JavaScript", "Matlab", "C", "C++", "Swift",
+        "Objective C", "Haskell", "Lisp", "Rust", "asdf"};
+        plList.setText("");
         for(String name : names) {
             plList.append(name + '\n');
 
